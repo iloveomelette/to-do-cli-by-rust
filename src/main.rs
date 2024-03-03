652263547a269407657e4ca154181c7b2068617a
iloveomelette <mizuki1998hosei@gmail.com>
2024-03-03 11:09:16 +0900

    :sparkles: Chapter9~10: use a default journal file to store tasks and display friendly error messages

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,26 @@ mod tasks;
 use structopt::StructOpt;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
+use std::path::PathBuf;
+use anyhow::anyhow;
 
-fn main() {
+/*
+ * Call the `home::home_dir()` function to obtain the path to the current user's home directory.
+ * The function returns an `Option<PathBuf>` type.
+ * `Some(PathBuf)` is returned if the home directory is found, `None` if not found.
+ *
+ * If the path to the home directory is found (`Some(PathBuf)`),
+ * the `.map` method is used on that path to add the file name `.rusty-journal.json` to the path.
+ * The `.map` method performs processing only if Option is `Some`, and does nothing if Option is `None`.
+ */
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+
+fn main() -> anyhow::Result<()> {
     /*
      * Get the command-line arguments.
      */
@@ -22,12 +40,15 @@ fn main() {
     /*
      * Unpack the journal file.
      */
-    let journal_file = journal_file.expect("Failed to find journal file.");
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
 
     match action {
         Add { task } => tasks::add_task(journal_file, Task::new(task)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action.")
+    }?;
+
+    Ok(())
 }
